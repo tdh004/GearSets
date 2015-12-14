27793a39d63fe62252d81b26aa77bfa9ed1802a4
T. Holmes <tdholmes@gmail.com>
2015-12-14 12:36:52 -0500

    Create sam_gear.lua

---
+++ sam_gear.lua
@@ -0,0 +1,18 @@
+function user_setup()
+
+  sets.precast.JA['Meikyo Shisui'] =
+  sets.precast.JA['Warding Circle'] =
+  sets.precast.JA['Third Eye'] =
+  sets.precast.JA['Hasso'] = {leg="Unkai haidate +2"}
+  sets.precast.JA['Meditate'] = {head="Wakido Kabuto +1,hands="Sakonji Kote"}
+  sets.precast.JA['Seigan'] =
+  sets.precast.JA['Sekkanoki'] =
+  sets.precast.JA['Konzen-ittai'] =
+  sets.precast.JA['Blade Bash'] =
+  sets.precast.JA['Shikikoyo'] =
+  sets.precast.JA['Sengikori'] =
+  sets.precast.JA['Hamanoha'] =
+  sets.precast.JA['Hagakure'] =
+  sets.precast.JA['Yaegasumi'] =
+  
+end
