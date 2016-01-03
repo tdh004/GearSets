455af8ec48827292e088ddaa2e6f2d98ca31d77f
T. Holmes <tdholmes@gmail.com>
2016-01-03 16:56:18 -0500

    Create war_gear.lua
    Adding basic Job Ability settings.

---
+++ war_gear.lua
@@ -0,0 +1,16 @@
+  function init_gear_sets ()
+  
+    sets.precast.JA['Mighty Strikes
+    sets.precast.JA['Provoke
+    sets.precast.JA['Berserk']  = {feet="Warrior's Calligae"}
+    sets.precast.JA['Defender
+    sets.precast.JA['Warcry'] = {head='Warrior's Mask +1'}
+    sets.precast.JA['Aggressor']  = {body="Warrior's Lorica +1"}
+    sets.precast.JA['Retaliation'] = {feet="Ravager's Calligae +2"}
+    sets.precast.JA['Warrior's Charge
+    sets.precast.JA['Tomahawk = {ammo=""}
+    sets.precast.JA['Restraint'] = {hand="Ravager's mufflers +2"}
+    sets.precast.JA['Blood Rage'] = {body="Ravager's lorica +2"}
+    sets.precast.JA['Brazen Rush
+    
+    end
